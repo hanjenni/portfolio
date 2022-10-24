f3aa98a801e0f0e522f943dc1a21a3935db46193
username <hanjenni@gmail.com>
2022-10-24 16:08:57 -0700

    contact email form

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,8 @@ export default function About(){
     <div>
         <h1>Hi, I'm Jen</h1>
         <p>I'm a teacher turned software engineer. I love creating functional and aesthetic applications! 
-            My spark for Web Development started when I taught 6th grade at a Computer Science Magnet School.  </p>
+            My spark for Web Development started when I taught 6th grade at a Computer Science Magnet School. 
+            Unfortunately, Covid hit and we couldn't learn as a class but I kept going and here I am now!  </p>
         <img className='myPic' src="https://i.imgur.com/uQMdJWmm.png" alt="Jennifer Han" />
     </div>
 
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './Contact.css'
 
-export default function Contact(){
+const Contact = () => {
+    const form = useRef()
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_zi09qql', 'template_oueit1y', form.current, 'FRxRwbrB8Imx6Pf3w')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      
+            
+      };
     return (
-        <h1>Contact</h1>
+        <>
+            <section>
+                <div className='container'>
+                    <h1>Contact</h1>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div>
+                            <input type="text" placeholder='name' name='user_name' required />
+                        </div>
+                        <div>
+                            <input type="email" placeholder='email' name='user_email' required />
+                        </div>
+                        <div>
+                            <input type="text" placeholder='subject' name='subject' required />
+                        </div>
+                        <div>
+                            <textarea name='message'></textarea>
+                        </div>
+                        <div>
+                            <button type='submit'>Send Message</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+
+        </>
     )
 }
+
+export default Contact
